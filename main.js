00bb706e19d40945ebe367ed0f5d9e4fe8c80da2
AshlyRomero <126489849+AshlyRomero@users.noreply.github.com>
2024-02-21 13:15:51 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+//Cartas
+let cards1 = ["🐶","🐱","🐭","🐹","🐰","🦊","🐻","🐼","🐻‍❄️","🐨","🐯","🦁"]
+
+let cards2 = ["🐶","🐱","🐭","🐹","🐰","🦊","🐻","🐼","🐻‍❄️","🐨","🐯","🦁"]
+
+let todas_las_tarjetas = cards1.concat(cards2);     
+
+
+function repartir_cartas(){
+
+    let tablero = document.querySelector("#root");
+
+    todas_las_tarjetas.forEach((cada_carta)=> {
+
+        let cart = document.createElement("div");
+        cart.innerHTML = `<div class="carta">${cada_carta}</div>`;
+
+        tablero.appendChild(cart);
+    });    
+}
+
+repartir_cartas();
